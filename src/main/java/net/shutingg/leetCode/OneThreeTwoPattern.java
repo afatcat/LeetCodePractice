a35c598135092fab865783fadcc3b55068747726
Shuting Guan <afatcat@gmail.com>
2017-10-19 21:19:25 -0400

    132 pattern done and passed

--- src/main/java/net/shutingg/leetCode/OneThreeTwoPattern.java
+++ src/main/java/net/shutingg/leetCode/OneThreeTwoPattern.java
@@ -4,4 +4,23 @@ package net.shutingg.leetCode;
  * Created by sguan on 10/19/17.
  */
 public class OneThreeTwoPattern {
+    public boolean find132pattern(int[] nums) {
+        if(nums==null || nums.length<3){
+            return false;
+        }
+        int numLeft = nums[0];
+        Integer numMiddle = null;
+        for(int i=1;i<nums.length;i++){
+            if(numMiddle == null && numLeft<nums[i]){
+                numMiddle = nums[i];
+            }else if(numMiddle == null && numLeft>nums[i]){
+                numLeft = nums[i];
+            }else if(numMiddle != null && numMiddle<nums[i]){
+                numMiddle = nums[i];
+            }else if(numMiddle != null && numMiddle > nums[i]){
+                return true;
+            }
+        }
+        return false;
+    }
 }
---
+++ src/test/java/net/shutingg/leetCode/OneThreeTwoPatternTests.java
@@ -0,0 +1,44 @@
+package net.shutingg.leetCode;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class OneThreeTwoPatternTests {
+    private OneThreeTwoPattern oneThreeTwoPattern;
+
+    @Before
+    public void setup(){
+         oneThreeTwoPattern = new OneThreeTwoPattern();
+    }
+
+    @Test
+    public void testFind132pattern(){
+        int nums[] = {1,2,3,4};
+        assertFalse(oneThreeTwoPattern.find132pattern(nums));
+
+        int nums2[] = {3, 1, 4, 2};
+        assertTrue(oneThreeTwoPattern.find132pattern(nums2));
+
+        int nums3[] = {-1, 3, 2, 0};
+        assertTrue(oneThreeTwoPattern.find132pattern(nums3));
+
+        int nums4[] = {-1, -2, 0, 2, 0};
+        assertTrue(oneThreeTwoPattern.find132pattern(nums4));
+
+        int nums5[] = {-1, -2, 0, 2, 5};
+        assertFalse(oneThreeTwoPattern.find132pattern(nums5));
+
+        int nums6[] = {};
+        assertFalse(oneThreeTwoPattern.find132pattern(nums6));
+
+        int nums7[] = {4, -3};
+        assertFalse(oneThreeTwoPattern.find132pattern(nums7));
+
+        int nums8[] = {4, -3, 9, 6};
+        assertTrue(oneThreeTwoPattern.find132pattern(nums8));
+
+        int nums9[] = {4, -3, 0, 6};
+        assertFalse(oneThreeTwoPattern.find132pattern(nums9));
+    }
+}
