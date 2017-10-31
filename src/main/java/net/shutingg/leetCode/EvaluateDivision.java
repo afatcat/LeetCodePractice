e12a9974dc157be906dc2d33cf7380505f0810ce
Shuting Guan <afatcat@gmail.com>
2017-10-30 23:14:19 -0400

    EvaluateDivision bfs improved (no need to use index array)

--- src/main/java/net/shutingg/leetCode/EvaluateDivision.java
+++ src/main/java/net/shutingg/leetCode/EvaluateDivision.java
@@ -6,58 +6,41 @@ import java.util.*;
  * Created by sguan on 10/30/17.
  */
 public class EvaluateDivision {
-    //4 ms, beats 23.77%
+    //3 ms, beats 60.69%
     public double[] calcEquation(String[][] equations, double[] values, String[][] queries) {
-        Set<String> variablesSet = new HashSet<>();
-        for(int i=0;i<equations.length;i++){
-            for(int j=0;j<equations[0].length;j++){
-                variablesSet.add(equations[i][j]);
-            }
-        }
-        String[] variables = new String[variablesSet.size()];
-        int index = 0;
-        for(String var:variablesSet){
-            variables[index]=var;
-            index++;
-        }
-        Map<String, Integer> indexMap = new HashMap<>();
-        for(int i =0; i<variables.length; i++){
-            indexMap.put(variables[i], i);
-        }
-
-        Map<Integer, Map<Integer, Double>> equationMap = new HashMap<>();
+        Map<String, Map<String, Double>> equationMap = new HashMap<>();
         for(int i=0; i<equations.length; i++){
-            if(equationMap.containsKey(indexMap.get(equations[i][0]))){
-                Map<Integer, Double> neighbours = equationMap.get(indexMap.get(equations[i][0]));
-                neighbours.put(indexMap.get(equations[i][1]), values[i]);
+            if(equationMap.containsKey(equations[i][0])){
+                Map<String, Double> neighbours = equationMap.get(equations[i][0]);
+                neighbours.put(equations[i][1], values[i]);
             }else{
-                Map<Integer, Double> neighbours = new HashMap<>();
-                neighbours.put(indexMap.get(equations[i][1]), values[i]);
-                equationMap.put(indexMap.get(equations[i][0]), neighbours);
+                Map<String, Double> neighbours = new HashMap<>();
+                neighbours.put(equations[i][1], values[i]);
+                equationMap.put(equations[i][0], neighbours);
             }
-            if(equationMap.containsKey(indexMap.get(equations[i][1]))){
-                Map<Integer, Double> neighbours = equationMap.get(indexMap.get(equations[i][1]));
-                neighbours.put(indexMap.get(equations[i][0]), 1/values[i]);
+            if(equationMap.containsKey(equations[i][1])){
+                Map<String, Double> neighbours = equationMap.get(equations[i][1]);
+                neighbours.put(equations[i][0], 1/values[i]);
             }else{
-                Map<Integer, Double> neighbours = new HashMap<>();
-                neighbours.put(indexMap.get(equations[i][0]), 1/values[i]);
-                equationMap.put(indexMap.get(equations[i][1]), neighbours);
+                Map<String, Double> neighbours = new HashMap<>();
+                neighbours.put(equations[i][0], 1/values[i]);
+                equationMap.put(equations[i][1], neighbours);
             }
         }
 
         double[] results = new double[queries.length];
         for(int i=0; i<queries.length; i++){
-            if(!equationMap.containsKey(indexMap.get(queries[i][0])) || !equationMap.containsKey(indexMap.get(queries[i][1]))){
+            if(!equationMap.containsKey(queries[i][0]) || !equationMap.containsKey(queries[i][1])){
                 results[i]=-1;
             }else if(queries[i][0].equals(queries[i][1])){
                 results[i]=1;
             }else{
-                int start = indexMap.get(queries[i][0]);
-                int end = indexMap.get(queries[i][1]);
+                String start = queries[i][0];
+                String end = queries[i][1];
                 if(equationMap.get(start).containsKey(end)){
                     results[i] = equationMap.get(start).get(end);
                 }else{
-                    results[i] = bfs(start, end, equationMap, variables.length);
+                    results[i] = bfs(start, end, equationMap);
                 }
             }
         }
@@ -65,23 +48,23 @@ public class EvaluateDivision {
         return results;
     }
 
-    private double bfs(int start, int end, Map<Integer, Map<Integer, Double>> equationMap, int size){
-        boolean[] visited = new boolean[size];
-        double[] distances = new double[size];
+    private double bfs(String start, String end, Map<String, Map<String, Double>> equationMap){
+        Set<String> visited = new HashSet<>();
+        Map<String, Double> distanceMap = new HashMap<>();
 
-        LinkedList<Integer> queue = new LinkedList<>();
+        LinkedList<String> queue = new LinkedList<>();
         queue.add(start);
-        visited[start] = true;
+        visited.add(start);
         while(!queue.isEmpty()){
-            int q = queue.pop();
-            Map<Integer, Double> neighbours = equationMap.get(q);
+            String q = queue.pop();
+            Map<String, Double> neighbours = equationMap.get(q);
             if(neighbours.containsKey(end)){
-                return distances[q]==0?neighbours.get(end):neighbours.get(end)*distances[q];
+                return !distanceMap.containsKey(q)?neighbours.get(end):neighbours.get(end)*distanceMap.get(q);
             }else{
-                for(int v:neighbours.keySet()){
-                    if(!visited[v]){
-                        visited[v] = true;
-                        distances[v] = distances[q]==0?neighbours.get(v):neighbours.get(v)*distances[q];
+                for(String v:neighbours.keySet()){
+                    if(!visited.contains(v)){
+                        visited.add(v);
+                        distanceMap.put(v, !distanceMap.containsKey(q)?neighbours.get(v):neighbours.get(v)*distanceMap.get(q));
                         queue.add(v);
                     }
                 }
