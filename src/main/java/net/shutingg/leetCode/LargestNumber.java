6c0e979c01e293b2f51f5d8d96f7be76c08323bd
Shuting Guan <afatcat@gmail.com>
2017-11-09 21:25:11 -0500

    LargestNumber comment

--- src/main/java/net/shutingg/leetCode/LargestNumber.java
+++ src/main/java/net/shutingg/leetCode/LargestNumber.java
@@ -8,6 +8,9 @@ import java.util.stream.Collectors;
  */
 public class LargestNumber {
     public String largestNumber(int[] nums) {
+        //write comparator to compare the ints
+        //sort the array based on the comparator
+        //append the array to final string
         return Arrays.stream(nums).mapToObj(String::valueOf).collect(Collectors.toList()).stream().sorted((String s1, String s2) -> -(s1+s2).compareTo(s2+s1)).reduce("", (String s1, String s2)->!s1.equals("0")?s1+s2:s2);
     }
 }
