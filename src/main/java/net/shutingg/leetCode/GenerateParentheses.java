8373fc9698ec8df835d53bd1e92092e326b1bebe
Shuting Guan <afatcat@gmail.com>
2017-11-13 20:19:53 -0500

    GenerateParentheses change StringBuffer to just String

--- src/main/java/net/shutingg/leetCode/GenerateParentheses.java
+++ src/main/java/net/shutingg/leetCode/GenerateParentheses.java
@@ -2,37 +2,27 @@ package net.shutingg.leetCode;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class GenerateParentheses {
     public List<String> generateParenthesis(int n) {
-        List<StringBuffer> buffers = new ArrayList<>();
+        List<String> result = new ArrayList<>();
         if(n==0){
-            return buffers.stream().map(StringBuffer::toString).collect(Collectors.toList());
+            return result;
         }
-        StringBuffer sb = new StringBuffer();
-        buffers.add(sb);
-        generator(buffers, 0, n, sb);
-        List<String> result = buffers.stream().map(StringBuffer::toString).collect(Collectors.toList());
+        generator(result, 0, n, "");
         return result;
     }
 
-    private void generator(List<StringBuffer> buffers, int leftCount, int rightCount, StringBuffer sb){
+    private void generator(List<String> list, int leftCount, int rightCount, String str){
         if(rightCount > 0){
             if(leftCount > 0){
-                StringBuffer sb2 = new StringBuffer();
-                sb2.append(sb.toString());
-                sb2.append(")");
-                buffers.add(sb2);
-                generator(buffers, leftCount-1, rightCount, sb2);
+                generator(list, leftCount-1, rightCount, str+")");
             }
-            sb.append("(");
-            generator(buffers, leftCount+1, rightCount-1, sb);
+            generator(list, leftCount+1, rightCount-1, str+"(");
         }else if(leftCount >0){
-            sb.append(")");
-            generator(buffers, leftCount-1, rightCount, sb);
+            generator(list, leftCount-1, rightCount, str+")");
         }else{
-            return;
+            list.add(str);
         }
     }
 }
