10d753ae9381b1e2465b58dde43dabdd1d7e6261
Shuting Guan <afatcat@gmail.com>
2017-11-15 19:20:54 -0500

    Interleaving String

---
+++ src/main/java/net/shutingg/leetCode/InterleavingString.java
@@ -0,0 +1,39 @@
+package net.shutingg.leetCode;
+
+public class InterleavingString {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if(s1 == null){
+            if(s2 !=null){
+                return s2.equals(s3);
+            }else{
+                return s3 == null;
+            }
+        }else{
+            if(s2 == null){
+                return s1.equals(s3);
+            }
+        }
+
+        if(s1.length() + s2.length() != s3.length()){
+            return false;
+        }
+
+        boolean f[][] = new boolean[s1.length()+1][s2.length()+1];
+        f[0][0] = true;
+        for(int i=1; i<= s1.length(); i++){
+            f[i][0] = s1.substring(0, i).equals(s3.substring(0, i));
+        }
+        for(int j=1; j<= s2.length(); j++){
+            f[0][j] = s2.substring(0, j).equals(s3.substring(0, j));
+        }
+
+        for(int i=1; i<= s1.length(); i++){
+            for(int j=1; j<= s2.length(); j++){
+                f[i][j] = f[i-1][j] && s1.substring(i-1, i).equals(s3.substring(i+j-1, i+j))
+                        || f[i][j-1] && s2.substring(j-1, j).equals(s3.substring(i+j-1, i+j));
+            }
+        }
+
+        return f[s1.length()][s2.length()];
+    }
+}
