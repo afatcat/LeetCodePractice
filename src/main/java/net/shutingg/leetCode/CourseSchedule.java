f36ad84f059baa77224b7438236ba2ca8540667c
Shuting Guan <afatcat@gmail.com>
2017-11-22 23:04:12 -0500

    CourseSchedule no more repeat check

--- src/main/java/net/shutingg/leetCode/CourseSchedule.java
+++ src/main/java/net/shutingg/leetCode/CourseSchedule.java
@@ -13,15 +13,18 @@ public class CourseSchedule {
             graph[prerequisites[i][0]].add(prerequisites[i][1]);
         }
 
+        Set<Integer> checked = new HashSet<>();
         for(int i=0; i<numCourses; i++){
-            if(isLoop(graph, i, new HashSet<>())){
-                return false;
+            if(!checked.contains(i)){
+                if(isLoop(graph, i, new HashSet<>(), checked)){
+                    return false;
+                }
             }
         }
         return true;
     }
 
-    private boolean isLoop(List<Integer>[] graph, int u, Set<Integer> visited){
+    private boolean isLoop(List<Integer>[] graph, int u, Set<Integer> visited, Set<Integer> checked){
         if(visited.contains(u)){
             return true;
         }
@@ -31,56 +34,13 @@ public class CourseSchedule {
             visited.add(u);
             List<Integer> neighbors = graph[u];
             for(int v:neighbors){
-                if(isLoop(graph, v, visited)){
+                if(isLoop(graph, v, visited, checked)){
                     return true;
                 }
                 visited.remove(v);
+                checked.add(v);
             }
             return false;
         }
     }
-//    public boolean canFinish(int numCourses, int[][] prerequisites) {
-//        if(numCourses == 0){
-//            return true;
-//        }
-//
-//        int m = prerequisites.length;
-//        if(m == 0){
-//            return true;
-//        }
-//
-//        //construct
-//        List<Integer>[] listArray = new LinkedList[numCourses];
-//        for(int i = 0 ; i < m; i++){
-//            int loc = prerequisites[i][0];
-//            if(listArray[loc] == null){
-//                listArray[loc] = new LinkedList<>();
-//            }
-//            listArray[loc].add(prerequisites[i][1]);
-//        }
-//
-//        //search for circle
-//        boolean[] visited = new boolean[numCourses];
-//        boolean success = true;
-//        for(int i = 0; i < listArray.length; i++){
-//            success = success && dfs(listArray, visited, i);
-//        }
-//
-//        return success;
-//    }
-//
-//    private boolean dfs(List<Integer>[] listArray, boolean[] visited, int target){
-//        if(visited[target]){
-//            return false;
-//        }
-//        visited[target] = true;
-//        if(listArray[target]!=null){
-//            for(int i:listArray[target]){
-//                if(!dfs(listArray, visited, i)){
-//                    return false;
-//                }
-//            }
-//        }
-//        return true;
-//    }
 }
