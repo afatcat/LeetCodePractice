7d5cca44dfdc3d2525ca80104f144d19421aa524
Shuting Guan <afatcat@gmail.com>
2017-11-22 22:52:29 -0500

    CourseSchedule answer correct but exceeds time limit

--- src/main/java/net/shutingg/leetCode/CourseSchedule.java
+++ src/main/java/net/shutingg/leetCode/CourseSchedule.java
@@ -1,51 +1,86 @@
 package net.shutingg.leetCode;
 
-import java.util.List;
-import java.util.LinkedList;
+import java.util.*;
 
 public class CourseSchedule {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        if(numCourses == 0){
-            return true;
-        }
-
-        int m = prerequisites.length;
-        if(m == 0){
-            return true;
-        }
-
-        //construct
-        List<Integer>[] listArray = new LinkedList[numCourses];
-        for(int i = 0 ; i < m; i++){
-            int loc = prerequisites[i][0];
-            if(listArray[loc] == null){
-                listArray[loc] = new LinkedList<>();
+        //construct graph
+        List<Integer>[] graph = new ArrayList[numCourses];
+        for(int i=0; i<prerequisites.length; i++){
+            if(graph[prerequisites[i][0]] == null){
+                graph[prerequisites[i][0]] = new ArrayList<>();
             }
-            listArray[loc].add(prerequisites[i][1]);
+            graph[prerequisites[i][0]].add(prerequisites[i][1]);
         }
 
-        //search for circle
-        boolean[] visited = new boolean[numCourses];
-        boolean success = true;
-        for(int i = 0; i < listArray.length; i++){
-            success = success && dfs(listArray, visited, i);
-        }
-
-        return success;
-    }
-
-    private boolean dfs(List<Integer>[] listArray, boolean[] visited, int target){
-        if(visited[target]){
-            return false;
-        }
-        visited[target] = true;
-        if(listArray[target]!=null){
-            for(int i:listArray[target]){
-                if(!dfs(listArray, visited, i)){
-                    return false;
-                }
+        for(int i=0; i<numCourses; i++){
+            if(isLoop(graph, i, new HashSet<>())){
+                return false;
             }
         }
         return true;
     }
+
+    private boolean isLoop(List<Integer>[] graph, int u, Set<Integer> visited){
+        if(visited.contains(u)){
+            return true;
+        }
+        if(graph[u] == null){
+            return false;
+        }else{
+            visited.add(u);
+            List<Integer> neighbors = graph[u];
+            for(int v:neighbors){
+                if(isLoop(graph, v, visited)){
+                    return true;
+                }
+                visited.remove(v);
+            }
+            return false;
+        }
+    }
+//    public boolean canFinish(int numCourses, int[][] prerequisites) {
+//        if(numCourses == 0){
+//            return true;
+//        }
+//
+//        int m = prerequisites.length;
+//        if(m == 0){
+//            return true;
+//        }
+//
+//        //construct
+//        List<Integer>[] listArray = new LinkedList[numCourses];
+//        for(int i = 0 ; i < m; i++){
+//            int loc = prerequisites[i][0];
+//            if(listArray[loc] == null){
+//                listArray[loc] = new LinkedList<>();
+//            }
+//            listArray[loc].add(prerequisites[i][1]);
+//        }
+//
+//        //search for circle
+//        boolean[] visited = new boolean[numCourses];
+//        boolean success = true;
+//        for(int i = 0; i < listArray.length; i++){
+//            success = success && dfs(listArray, visited, i);
+//        }
+//
+//        return success;
+//    }
+//
+//    private boolean dfs(List<Integer>[] listArray, boolean[] visited, int target){
+//        if(visited[target]){
+//            return false;
+//        }
+//        visited[target] = true;
+//        if(listArray[target]!=null){
+//            for(int i:listArray[target]){
+//                if(!dfs(listArray, visited, i)){
+//                    return false;
+//                }
+//            }
+//        }
+//        return true;
+//    }
 }
--- src/test/java/net/shutingg/leetCode/CourseScheduleTests.java
+++ src/test/java/net/shutingg/leetCode/CourseScheduleTests.java
@@ -17,10 +17,20 @@ public class CourseScheduleTests {
         input[0] = new int[]{1,0};
         input[1] = new int[]{2,0};
         input[2] = new int[]{1,2};
-        assertFalse(courseSchedule.canFinish(3, input));
+        assertTrue(courseSchedule.canFinish(3, input));
 
         input = new int[1][2];
         input[0] = new int[]{1,0};
         assertTrue(courseSchedule.canFinish(2, input));
+
+        int[][] input3 = {
+                {1, 0},
+                {2, 6},
+                {1, 7},
+                {6, 4},
+                {7, 0},
+                {0, 5}
+        };
+        assertTrue(courseSchedule.canFinish(8, input3));
     }
 }
