124edf2320d2228262da857468fcc4becf987c5b
Shuting Guan <afatcat@gmail.com>
2017-12-15 22:08:29 -0500

    InterleavingString clean code

--- src/main/java/net/shutingg/leetCode/InterleavingString.java
+++ src/main/java/net/shutingg/leetCode/InterleavingString.java
@@ -1,39 +1,48 @@
 package net.shutingg.leetCode;
 
+/**
+ * https://leetcode.com/problems/interleaving-string/description/
+ * http://www.lintcode.com/en/problem/interleaving-string/
+ */
 public class InterleavingString {
+    /**
+     * DP - double sequence
+     *
+     * @param s1: A string
+     * @param s2: A string
+     * @param s3: A string
+     * @return: Determine whether s3 is formed by interleaving of s1 and s2
+     */
     public boolean isInterleave(String s1, String s2, String s3) {
-        if(s1 == null){
-            if(s2 !=null){
-                return s2.equals(s3);
-            }else{
-                return s3 == null;
-            }
-        }else{
-            if(s2 == null){
-                return s1.equals(s3);
-            }
-        }
-
-        if(s1.length() + s2.length() != s3.length()){
+        if(s1 == null || s2 == null || s3 == null){
             return false;
         }
 
-        boolean f[][] = new boolean[s1.length()+1][s2.length()+1];
-        f[0][0] = true;
-        for(int i=1; i<= s1.length(); i++){
-            f[i][0] = s1.substring(0, i).equals(s3.substring(0, i));
+        int m = s1.length();
+        int n = s2.length();
+        if(m+n != s3.length()){
+            return false;
         }
-        for(int j=1; j<= s2.length(); j++){
-            f[0][j] = s2.substring(0, j).equals(s3.substring(0, j));
+        char[] A = s1.toCharArray();
+        char[] B = s2.toCharArray();
+        char[] C = s3.toCharArray();
+
+        boolean[][] f = new boolean[m+1][n+1];
+        f[0][0] = true;
+        for(int i = 1; i <= m; i++){
+            f[i][0] = A[i-1] == C[i-1];
+        }
+        for(int j = 1; j <= n; j++){
+            f[0][j] = B[j-1] == C[j-1];
         }
 
-        for(int i=1; i<= s1.length(); i++){
-            for(int j=1; j<= s2.length(); j++){
-                f[i][j] = f[i-1][j] && s1.charAt(i-1) == s3.charAt(i+j-1)
-                        || f[i][j-1] && s2.charAt(j-1) == s3.charAt(i+j-1);
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                f[i][j] = f[i-1][j] && A[i-1] == C[i+j-1];
+                f[i][j] |= f[i][j-1] && B[j-1] == C[i+j-1];
             }
         }
 
-        return f[s1.length()][s2.length()];
+        return f[m][n];
     }
 }
