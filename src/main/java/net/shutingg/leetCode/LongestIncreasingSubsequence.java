f53e44c557dba9851114b37153379f1c63f6219b
Shuting Guan <afatcat@gmail.com>
2018-01-08 23:15:15 -0500

    LongestIncreasingSubsequence O(NlogN)

--- src/main/java/net/shutingg/leetCode/LongestIncreasingSubsequence.java
+++ src/main/java/net/shutingg/leetCode/LongestIncreasingSubsequence.java
@@ -2,6 +2,40 @@ package net.shutingg.leetCode;
 
 public class LongestIncreasingSubsequence {
     /**
+     * DP - O(N*logN)
+     * @param nums: An integer array
+     * @return: The length of LIS (longest increasing subsequence)
+     */
+    public int longestIncreasingSubsequence2(int[] nums) {
+        int n = nums.length;
+        if(n == 0) return 0;
+        int[] b = new int[n+1];
+        b[0] = Integer.MIN_VALUE;
+        int btop = 0;
+        int start, stop;
+
+        for(int i = 0; i < n; i++){
+            start = 0;
+            stop = btop;
+            int p = (start + stop) / 2;
+            while(start <= stop){
+                if(b[p] < nums[i]){
+                    start = p+1;
+                }else{
+                    stop = p-1;
+                }
+                p = (start + stop) / 2;
+            }
+            b[p+1] = nums[i];
+            if(p+1 > btop){
+                btop++;
+            }
+        }
+
+        return btop;
+    }
+
+    /**
      * DP - O(N^2)
      * @param nums: An integer array
      * @return: The length of LIS (longest increasing subsequence)
