26526f88b63f2a7b253161a543b7ef91cb2a24cd
Shuting Guan <afatcat@gmail.com>
2018-01-20 22:49:28 -0500

    LongestIncreasingContinuousSubsequence and UniquePathsII

---
+++ src/main/java/net/shutingg/leetCode/LongestIncreasingContinuousSubsequence.java
@@ -0,0 +1,49 @@
+package net.shutingg.leetCode;
+
+/**
+ * http://www.lintcode.com/en/problem/longest-increasing-continuous-subsequence/
+ */
+public class LongestIncreasingContinuousSubsequence {
+    /**
+     * DP - coordinate
+     * @param A: An array of Integer
+     * @return: an integer
+     */
+    public int longestIncreasingContinuousSubsequence(int[] A) {
+        // f[i] = f[i-1] + 1 | A[i] > A[i-1]
+        // g[i] = 1
+        //OR
+        // g[i] = g[i-1] + 1 | A[i] > A[i-1]
+        // f[i] = 1
+
+        if(A == null || A.length == 0){
+            return 0;
+        }
+        int n = A.length;
+        int[] f = new int[n];
+        int[] g = new int[n];
+        f[0] = 1;
+        g[0] = 1;
+        for(int i = 1; i < n; i++){
+            if(A[i] > A[i-1]){
+                f[i] = f[i-1] + 1;
+                g[i] = 1;
+                continue;
+            }
+            if(A[i] < A[i-1]){
+                g[i] = g[i-1] + 1;
+                f[i] = 1;
+                continue;
+            }
+            f[i] = 1;
+            g[i] = 1;
+        }
+
+        int res = 0;
+        for(int i = 0; i < n; i++){
+            res = Math.max(res, f[i]);
+            res = Math.max(res, g[i]);
+        }
+        return res;
+    }
+}
---
+++ src/main/java/net/shutingg/leetCode/UniquePathsII.java
@@ -0,0 +1,49 @@
+package net.shutingg.leetCode;
+
+/**
+ * http://www.lintcode.com/en/problem/unique-paths-ii/
+ */
+public class UniquePathsII {
+    /**
+     * DP - coordinate
+     * @param obstacleGrid: A list of lists of integers
+     * @return: An integer
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        // f[i][j] = f[i-1][j] + f[i][j-1] | obstacleGrid[i][j] = 0
+        // OR f[i][j] = 0;
+        if(obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0].length == 0){
+            return 0;
+        }
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        int[][] f = new int[m][n];
+        if(obstacleGrid[0][0] == 1){
+            return 0;
+        }
+        f[0][0] = 1;
+        for(int i = 1; i < m; i++){
+            if(obstacleGrid[i][0] == 1){
+                break;
+            }
+            f[i][0] = f[i-1][0];
+        }
+        for(int j = 1; j < n; j++){
+            if(obstacleGrid[0][j] == 1){
+                break;
+            }
+            f[0][j] = f[0][j-1];
+        }
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(obstacleGrid[i][j] == 1){
+                    f[i][j] = 0;
+                    continue;
+                }
+                f[i][j] = f[i][j-1] + f[i-1][j];
+            }
+        }
+        return f[m-1][n-1];
+    }
+}
