d3662da97ef92f271e0952fe06db9f1e68203122
Shuting Guan <afatcat@gmail.com>
2018-01-22 09:46:27 -0500

    First Bad Version

---
+++ src/main/java/net/shutingg/leetCode/FirstBadVersion.java
@@ -0,0 +1,37 @@
+package net.shutingg.leetCode;
+
+public class FirstBadVersion {
+    /*
+     * @param n: An integer
+     * @return: An integer which is the first bad version.
+     */
+    public int findFirstBadVersion(int n) {
+        int st = 1;
+        int end = n;
+        int p = st + (end - st) / 2;
+        while (st < end){
+            if (SVNRepo.isBadVersion(p)) {
+                end = p;
+                p = st + (end - st) / 2;
+            }else{
+                st = p + 1;
+                p = st + (end - st) / 2;
+            }
+        }
+
+        return p;
+    }
+}
+
+class SVNRepo{
+    static int BAD_VERSION = 10;
+
+    static boolean isBadVersion(int v){
+        //some definition
+        if(v >= BAD_VERSION){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
