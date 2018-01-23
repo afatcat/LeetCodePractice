f4a1537bc1a1a92d962daab2c04eb349aac3ebdf
Shuting Guan <afatcat@gmail.com>
2018-01-22 22:34:52 -0500

    SearchInsertPosition

---
+++ src/main/java/net/shutingg/leetCode/SearchInsertPosition.java
@@ -0,0 +1,40 @@
+package net.shutingg.leetCode;
+
+/**
+ * http://lintcode.com/en/problem/search-insert-position/
+ */
+public class SearchInsertPosition {
+    /**
+     * Binary Search
+     * @param A: an integer sorted array
+     * @param target: an integer to be inserted
+     * @return: An integer
+     */
+    public int searchInsert(int[] A, int target) {
+        int n = A.length;
+        if (n == 0){
+            return 0;
+        }
+        int st = 0;
+        int end = n - 1;
+        int p = st + (end - st) / 2;
+
+        while (st < end){
+            if (A[p] == target){
+                return p;
+            } else if (A[p] < target) {
+                st = p + 1;
+                p = st + (end - st) / 2;
+            } else {
+                end = p;
+                p = st + (end - st) / 2;
+            }
+        }
+
+        if(A[p] < target){
+            return p + 1;
+        }
+
+        return p;
+    }
+}
