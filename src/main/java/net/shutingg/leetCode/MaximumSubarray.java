b0a211bc0bb3743a4e714a50fa1343ae09cc7f91
sguan <shuting.guan@teachforamerica.org>
2018-01-25 17:56:55 -0500

    MaximumSubarray

---
+++ src/main/java/net/shutingg/leetCode/MaximumSubarray.java
@@ -0,0 +1,40 @@
+package net.shutingg.leetCode;
+
+/**
+ * http://www.lintcode.com/en/problem/maximum-subarray/
+ */
+public class MaximumSubarray {
+    /**
+     * DP - O(n^2)
+     * @param nums: A list of integers
+     * @return: A integer indicate the sum of max subarray
+     */
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        if(n == 0){
+            return 0;
+        }
+
+        int[] f = new int[n];
+        int res = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++){
+            f[i] = nums[i];
+            if(f[i] > res) {
+                res = f[i];
+            }
+        }
+        for(int i = 0; i < n; i++){
+            if(nums[i] <= 0) {
+                continue;
+            }
+            f[i] = nums[i];
+            for(int j = i + 1; j < n; j++){
+                f[j] = f[j-1] + nums[j];
+                if(f[j] > res) {
+                    res = f[j];
+                }
+            }
+        }
+        return res;
+    }
+}
