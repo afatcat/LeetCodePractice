9f11b0c4b3aa312a30a02838f939842f6d908723
Shuting Guan <afatcat@gmail.com>
2018-01-29 23:09:08 -0500

    Subsets another backtracking solution

--- src/main/java/net/shutingg/leetCode/Subsets.java
+++ src/main/java/net/shutingg/leetCode/Subsets.java
@@ -1,9 +1,45 @@
 package net.shutingg.leetCode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Subsets {
+    /**
+     * backtracking - with remove
+     * @param nums: A set of numbers
+     * @return: A list of lists
+     */
+    public List<List<Integer>> subsets2(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(nums == null || nums.length == 0) {
+            result.add(new ArrayList<>());
+            return result;
+        }
+
+        Arrays.sort(nums);
+        helper(result, new ArrayList<>(), nums, 0);
+        return result;
+    }
+
+    private void helper(List<List<Integer>> result, List<Integer> list, int[] nums, int st) {
+        result.add(new ArrayList(list));
+        if (list.size() == nums.length) {
+            return;
+        }
+
+        for(int i = st; i < nums.length; i++) {
+            list.add(nums[i]);
+            helper(result, list, nums, i + 1);
+            list.remove(list.size() - 1);
+        }
+    }
+
+    /**
+     * Backtracking my way
+     * @param nums
+     * @return
+     */
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         for(int i = 0; i <nums.length; i++){
