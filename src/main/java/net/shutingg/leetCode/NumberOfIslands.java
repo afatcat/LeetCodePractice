0faec423f69904c65680d7a895354ab5527be470
sguan <shuting.guan@teachforamerica.org>
2018-02-05 17:59:40 -0500

    NumberOfIslands UnionFind

--- src/main/java/net/shutingg/leetCode/NumberOfIslands.java
+++ src/main/java/net/shutingg/leetCode/NumberOfIslands.java
@@ -1,6 +1,89 @@
 package net.shutingg.leetCode;
 
 public class NumberOfIslands {
+    /**
+     * UnionFind
+     * http://www.lintcode.com/en/problem/number-of-islands/
+     * http://www.jiuzhang.com/solution/number-of-islands/
+     *
+     * @param grid: a boolean 2D matrix
+     * @return: an integer
+     */
+    public int numIslands(boolean[][] grid) {
+        if (grid == null) {
+            return 0;
+        }
+
+        int m = grid.length;
+        if (m == 0) {
+            return 0;
+        }
+        int n = grid[0].length;
+        UnionFind uf = new UnionFind(m * n);
+
+        //count 0
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(grid[i][j]) {
+                    count++;
+                }
+            }
+        }
+        uf.count = count;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j]) {
+                    //look down
+                    if (i < m - 1 && grid[i+1][j]) {
+                        uf.union(i * n + j, (i + 1) * n + j);
+                    }
+                    //look right
+                    if (j < n - 1 && grid[i][j+1]) {
+                        uf.union(i * n + j, i * n + (j + 1));
+                    }
+                }
+            }
+        }
+
+        return uf.count;
+    }
+
+    private class UnionFind {
+        int[] fathers = null;
+        int count;
+
+        UnionFind (int n) {
+            fathers = new int[n];
+            for (int i = 0; i < n; i++) {
+                fathers[i] = i;
+            }
+        }
+
+        int find (int x) {
+            if (fathers[x] == x) {
+                return x;
+            }
+            fathers[x] = find(fathers[x]);
+            return fathers[x];
+        }
+
+        void union (int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX != rootY) {
+                fathers[rootY] = rootX;
+                count--;
+            }
+        }
+    }
+
+    /**
+     * DFS
+     * @param grid
+     * @return
+     */
     public int numIslands(char[][] grid) {
         if(grid == null || grid.length == 0 || grid[0].length ==0){
             return 0;
