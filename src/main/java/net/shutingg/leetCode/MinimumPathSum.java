ecf170859407a541cacde869f1bc2ac006e66b19
Shuting Guan <afatcat@gmail.com>
2018-02-28 00:38:34 -0500

    MinimumPathSum

---
+++ src/main/java/net/shutingg/leetCode/MinimumPathSum.java
@@ -0,0 +1,35 @@
+package net.shutingg.leetCode;
+
+public class MinimumPathSum {
+    /**
+     * DP
+     *
+     * @param grid: a list of lists of integers
+     * @return: An integer, minimizes the sum of all numbers along its path
+     */
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+
+        //f[i][j] = min(f[i-1][j], f[i][j-1]) + grid[i][j]
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] f = new int[m][n];
+        f[0][0] = grid[0][0];
+        for (int i = 1; i < m; i++) {
+            f[i][0] = f[i - 1][0] + grid[i][0];
+        }
+        for (int j = 1; j < n; j++) {
+            f[0][j] = f[0][j - 1] + grid[0][j];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                f[i][j] = Math.min(f[i - 1][j], f[i][j - 1]) + grid[i][j];
+            }
+        }
+
+        return f[m - 1][n - 1];
+    }
+}
