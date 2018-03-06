3edcc4cc538c07a9b0c1953a4465971044ce6343
Shuting Guan <afatcat@gmail.com>
2018-03-06 01:00:38 -0500

    CourseSchedule BFS

--- src/main/java/net/shutingg/leetCode/CourseSchedule.java
+++ src/main/java/net/shutingg/leetCode/CourseSchedule.java
@@ -2,7 +2,61 @@ package net.shutingg.leetCode;
 
 import java.util.*;
 
+/**
+ * http://www.lintcode.com/en/problem/course-schedule/
+ */
 public class CourseSchedule {
+    /**
+     * BFS
+     *
+     * @param numCourses: a total of n courses
+     * @param prerequisites: a list of prerequisite pairs
+     * @return: true if can finish all courses or false
+     */
+    public boolean canFinish2(int numCourses, int[][] prerequisites) {
+        if (prerequisites == null || prerequisites.length == 0) {
+            return true;
+        }
+
+        List<Integer>[] lists = new ArrayList[numCourses];
+        int[] degree = new int[numCourses];
+        int m = prerequisites.length;
+        for (int i = 0; i < numCourses; i++) {
+            lists[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < m; i++) {
+            lists[prerequisites[i][1]].add(prerequisites[i][0]);
+            degree[prerequisites[i][0]]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < degree.length; i++) {
+            if (degree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        int count = 0;
+        while (!queue.isEmpty()) {
+            count++;
+            int a = queue.poll();
+            for (Integer b : lists[a]) {
+                degree[b]--;
+                if (degree[b] == 0) {
+                    queue.offer(b);
+                }
+            }
+        }
+
+        return count == numCourses;
+    }
+
+    /**
+     * DFS
+     *
+     * @param numCourses
+     * @param prerequisites
+     * @return
+     */
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         //construct graph
         List<Integer>[] graph = new ArrayList[numCourses];
