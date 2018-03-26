887dad5baa1f56c7287726a8c51e9d20fff60602
Shuting Guan <afatcat@gmail.com>
2018-03-25 21:47:22 -0400

    MoveZeroes two pointers

--- src/main/java/net/shutingg/leetCode/MoveZeroes.java
+++ src/main/java/net/shutingg/leetCode/MoveZeroes.java
@@ -5,6 +5,8 @@ package net.shutingg.leetCode;
  */
 public class MoveZeroes {
     /**
+     * Two pointers - same direction
+     *
      * @param nums: an integer array
      * @return: nothing
      */
@@ -13,21 +15,16 @@ public class MoveZeroes {
             return;
         }
 
-        int j = nums.length - 1;
-        while (j >= 0 && nums[j] == 0) {
-            j--;
-        }
-
-        int i = j - 1;
-        while (i >= 0) {
-            if (nums[i] == 0) {
-                for (int k = i; k < j; k++) {
-                    nums[k] = nums[k + 1];
-                }
-                nums[j] = 0;
-                j--;
+        int i = 0;
+        int j = 0;
+        while (j < nums.length) {
+            if (nums[j] != 0) {
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+                i++;
             }
-            i--;
+            j++;
         }
     }
 }
