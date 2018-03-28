ac8ca41b6c6b742488a81b6b466675b45491d32c
Shuting Guan <afatcat@gmail.com>
2018-03-27 23:27:33 -0400

    KthSmallestNumbersInUnsortedArray quick select

--- src/main/java/net/shutingg/leetCode/KthSmallestNumbersInUnsortedArray.java
+++ src/main/java/net/shutingg/leetCode/KthSmallestNumbersInUnsortedArray.java
@@ -7,7 +7,60 @@ import java.util.PriorityQueue;
  */
 public class KthSmallestNumbersInUnsortedArray {
     /**
+     * Quick Select
+     * O(n)
+     *
+     * @param k: An integer
+     * @param nums: An integer array
+     * @return: kth smallest element
+     */
+    public int kthSmallest2(int k, int[] nums) {
+        if (nums == null || nums.length < k || k <= 0) {
+            return Integer.MIN_VALUE;
+        }
+
+        return quickSelect(k, nums, 0, nums.length - 1);
+    }
+
+    private int quickSelect(int k, int[] nums, int st, int end) {
+        if (st == end) {
+            return nums[st];
+        }
+
+        int pivot = nums[(st + end) / 2];
+        int i = st;
+        int j = end;
+        while (i <= j) {
+            while (i <= j && nums[i] < pivot) {
+                i++;
+            }
+
+            while (i <= j && nums[j] > pivot) {
+                j--;
+            }
+
+            if (i <= j) {
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+                i++;
+                j--;
+            }
+        }
+
+        if (j >= k - 1) {
+            return quickSelect(k, nums, st, j);
+        }
+        if (i <= k - 1) {
+            return quickSelect(k, nums, i, end);
+        }
+        return nums[j + 1];
+    }
+
+
+    /**
      * PriorityQueue
+     * O(n log n)
      *
      * @param k: An integer
      * @param nums: An integer array
