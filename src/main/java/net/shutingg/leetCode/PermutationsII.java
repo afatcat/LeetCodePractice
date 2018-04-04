ab0c6acc467a019a34343ddde6b40d81cddfc012
Shuting Guan <afatcat@gmail.com>
2018-04-04 19:17:19 -0400

    PermutationsII next permutation

--- src/main/java/net/shutingg/leetCode/PermutationsII.java
+++ src/main/java/net/shutingg/leetCode/PermutationsII.java
@@ -6,8 +6,89 @@ import java.util.*;
  * https://leetcode.com/problems/permutations-ii
  */
 public class PermutationsII {
-    /*
-    Backtracking solution
+    private boolean end = false;
+    /**
+     * Next Permutation
+     *
+     * @param :  A list of integers
+     * @return: A list of unique permutations
+     */
+    public List<List<Integer>> permuteUnique2(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null) {
+            return res;
+        }
+
+        if (nums.length <= 1) {
+            res.add(arrayToList(nums));
+            return res;
+        }
+
+        Arrays.sort(nums);
+
+        while (!end) {
+            res.add(arrayToList(nums));
+            nextP(nums);
+        }
+
+        return res;
+    }
+
+    private List<Integer> arrayToList(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            list.add(nums[i]);
+        }
+        return list;
+    }
+
+    private void nextP(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0) {
+            if (nums[i] < nums[i + 1]) {
+                break;
+            }
+            i--;
+        }
+        if (i < 0) {
+            end = true;
+            return;
+        }
+        int j = nums.length - 1;
+        while (j > i) {
+            if (nums[j] > nums[i]) {
+                swap(nums, i, j);
+                flip(nums, i + 1, nums.length - 1);
+                break;
+            }
+            j--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+    private void flip(int[] nums, int i, int j) {
+        if (i >= j) {
+            return;
+        }
+
+        while (i < j) {
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+            i++;
+            j--;
+        }
+    }
+
+
+
+    /**
+     * Backtracking solution
      */
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
