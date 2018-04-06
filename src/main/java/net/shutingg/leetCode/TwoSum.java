60cdf76f1c4301072f8bb4029ba13b2de9f429db
Shuting Guan <afatcat@gmail.com>
2018-04-05 23:10:07 -0400

    TwoSum hash

--- src/main/java/net/shutingg/leetCode/TwoSum.java
+++ src/main/java/net/shutingg/leetCode/TwoSum.java
@@ -1,6 +1,41 @@
 package net.shutingg.leetCode;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * http://www.lintcode.com/en/problem/two-sum/
+ */
 public class TwoSum {
+    /**
+     * Hash
+     *
+     * @param numbers: An array of Integer
+     * @param target: target = numbers[index1] + numbers[index2]
+     * @return: [index1 + 1, index2 + 1] (index1 < index2)
+     */
+    public int[] twoSum2(int[] numbers, int target) {
+        if (numbers == null || numbers.length <= 1) {
+            return new int[0];
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            int otherNum = target - numbers[i];
+            if (map.containsKey(otherNum)) {
+                int[] result = new int[2];
+                result[0] = map.get(otherNum);
+                result[1] = i;
+                return result;
+            } else {
+                map.put(numbers[i], i);
+            }
+        }
+
+        return new int[0];
+    }
+
+
     public int[] twoSum(int[] nums, int target) {
         if(nums.length <= 1){
             return null;
