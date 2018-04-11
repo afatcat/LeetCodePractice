5b52c0de281feef7cfa5a0fc4770adea08ff7fca
Shuting Guan <afatcat@gmail.com>
2018-04-10 20:21:38 -0400

    PaintHouse

---
+++ src/main/java/net/shutingg/leetCode/PaintHouse.java
@@ -0,0 +1,33 @@
+package net.shutingg.leetCode;
+
+/**
+ * http://www.lintcode.com/en/problem/paint-house/
+ */
+public class PaintHouse {
+    /**
+     * DP - sequence
+     *
+     * @param costs: n x 3 cost matrix
+     * @return: An integer, the minimum cost to paint all houses
+     */
+    public int minCost(int[][] costs) {
+        // f[i][0] = min(f[i - 1][1], f[i - 1][2]) + costs[i][0]
+
+        if (costs == null || costs.length == 0 || costs[0].length == 0) {
+            return 0;
+        }
+
+        int n = costs.length;
+        int[][] f = new int[n][3];
+        f[0][0] = costs[0][0];
+        f[0][1] = costs[0][1];
+        f[0][2] = costs[0][2];
+        for (int i = 1; i < n; i++) {
+            f[i][0] = Math.min(f[i - 1][1], f[i - 1][2]) + costs[i][0];
+            f[i][1] = Math.min(f[i - 1][0], f[i - 1][2]) + costs[i][1];
+            f[i][2] = Math.min(f[i - 1][0], f[i - 1][1]) + costs[i][2];
+        }
+
+        return Math.min(f[n - 1][0], Math.min(f[n - 1][1], f[n - 1][2]));
+    }
+}
