3af9dd875a44145f9bc17ad69d4770bb53146e26
Shuting Guan <afatcat@gmail.com>
2018-04-14 17:57:17 -0400

    MoveZeroes

--- src/main/java/net/shutingg/leetCode/MoveZeroes.java
+++ src/main/java/net/shutingg/leetCode/MoveZeroes.java
@@ -5,6 +5,38 @@ package net.shutingg.leetCode;
  */
 public class MoveZeroes {
     /**
+     * Two Pointer - same direction
+     * Keep order and minimize write (no swap)
+     *
+     * @param nums
+     */
+    public void moveZeroes2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+
+        int n = nums.length;
+        int i = 0;
+        while (i < n && nums[i] != 0) {
+            i++;
+        }
+        int j = i + 1;
+        while (j < n) {
+            if (nums[j] == 0) {
+                j++;
+                continue;
+            }
+            nums[i] = nums[j];
+            i++;
+            j++;
+        }
+        while (i < n) {
+            nums[i] = 0;
+            i++;
+        }
+    }
+
+    /**
      * Two pointers - same direction
      *
      * @param nums: an integer array
