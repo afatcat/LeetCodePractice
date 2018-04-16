dd2071ece6d0adde2b6b1657c0e58c899de81d6c
Shuting Guan <afatcat@gmail.com>
2018-04-16 00:25:04 -0400

    UglyNumberII PriorityQueue

--- src/main/java/net/shutingg/leetCode/UglyNumberII.java
+++ src/main/java/net/shutingg/leetCode/UglyNumberII.java
@@ -1,5 +1,9 @@
 package net.shutingg.leetCode;
 
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
 /**
  * https://leetcode.com/problems/ugly-number-ii/description/
  */
@@ -36,5 +40,39 @@ public class UglyNumberII {
             }
         }
         return f[n-1];
+    }
+
+    /**
+     * PriorityQueue O(n log n)
+     *
+     * @param n: An integer
+     * @return: the nth prime number as description.
+     */
+    public int nthUglyNumber2(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        Set<Long> inQ = new HashSet<>();
+        long[] primes = new long[]{2, 3, 5};
+        for (int i = 0; i < 3; i++) {
+            pq.add(primes[i]);
+            inQ.add(primes[i]);
+        }
+        for (int i = 2; i < n; i++) {
+            long current = pq.poll();
+            for (int j = 0; j < 3; j++) {
+                long next = current * primes[j];
+                if (!inQ.contains(next)) {
+                    pq.offer(next);
+                    inQ.add(next);
+                }
+            }
+        }
+
+        return ((Long) pq.poll()).intValue();
     }
 }
