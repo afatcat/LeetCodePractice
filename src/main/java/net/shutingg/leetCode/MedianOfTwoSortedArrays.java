ae072143fc9689c7399814486e640fa5372450f5
Shuting Guan <afatcat@gmail.com>
2018-04-17 13:25:30 -0400

    MedianOfTwoSortedArrays

--- src/main/java/net/shutingg/leetCode/MedianOfTwoSortedArrays.java
+++ src/main/java/net/shutingg/leetCode/MedianOfTwoSortedArrays.java
@@ -1,6 +1,116 @@
 package net.shutingg.leetCode;
 
+/**
+ * http://www.lintcode.com/en/problem/median-of-two-sorted-arrays/
+ */
 public class MedianOfTwoSortedArrays {
+    /**
+     * Binary Search
+     *
+     * @param A: An integer array
+     * @param B: An integer array
+     * @return: a double whose format is *.5 or *.0
+     */
+    public double findMedianSortedArrays2(int[] A, int[] B) {
+        if (A == null && B == null) {
+            return 0;
+        }
+        if (A == null) {
+            if (B.length == 0) {
+                return 0;
+            }
+            else if ((B.length & 1) == 1) {
+                return B[B.length / 2 + 1];
+            } else {
+                return (B[B.length / 2] + B[B.length / 2 + 1]) / 2.0;
+            }
+        }
+        if (B == null) {
+            if (A.length == 0) {
+                return 0;
+            } else if ((A.length & 1) == 1) {
+                return A[A.length / 2 + 1];
+            } else {
+                return (A[A.length /2] + A[A.length / 2 + 1]) / 2.0;
+            }
+        }
+        int total = A.length + B.length;
+        if ((total & 1) == 1) {
+            return findKthLargest(A, B, total / 2 + 1);
+        } else {
+            int left = findKthLargest(A, B, total / 2);
+            int right = findKthLargest(A, B, total / 2 + 1);
+            System.out.println("left: " + left +", right: " + right);
+            return (left + right) / 2.0;
+        }
+    }
+
+    private int findKthLargest(int[] arr1, int[] arr2, int k) {
+        if (arr1 == null && arr2 == null) {
+            return 0;
+        }
+        if (arr1 == null || arr1.length == 0) {
+            return arr2[arr2.length - k];
+        }
+        if (arr2 == null || arr2.length == 0) {
+            return arr1[arr1.length - k];
+        }
+
+        int min = Math.min(arr1[0], arr2[0]);
+        int max = Math.max(arr1[arr1.length - 1], arr2[arr2.length - 1]);
+        while (min + 1 < max) {
+            int pivot = (max - min) / 2 + min;
+            int count = countGEInTwo(arr1, arr2, pivot);
+            if (count < k) {
+                max = pivot;
+            } else {
+                min = pivot;
+            }
+        }
+
+        System.out.println("min: " + min + ", max: " + max);
+        if (countGEInTwo(arr1, arr2, max) >= k) {
+            System.out.println("k: " + k + ", " + max);
+            return max;
+        } else {
+            System.out.println("k: " + k + ", " + min);
+            return min;
+        }
+    }
+
+    private int countGEInTwo(int[] arr1, int[] arr2, int pivot) {
+        return countGE(arr1, pivot) + countGE(arr2, pivot);
+    }
+
+    private int countGE(int[] arr, int pivot) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+        int st = 0;
+        int end = arr.length - 1;
+        while (st + 1 < end) {
+            int mid = (end - st) / 2 + st;
+            if (arr[mid] >= pivot) {
+                end = mid;
+            } else {
+                st = mid;
+            }
+        }
+        if (arr[st] >= pivot) {
+            System.out.println(arr.length + " pivot: " + pivot + ", st: " + st);
+            return arr.length - st;
+        }
+        if (arr[end] >= pivot) {
+            System.out.println(arr.length + " pivot: " + pivot + ", end: " + end);
+            return arr.length - end;
+        }
+
+
+        return 0;
+    }
+
+
+
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int m = nums1.length;
         int n = nums2.length;
