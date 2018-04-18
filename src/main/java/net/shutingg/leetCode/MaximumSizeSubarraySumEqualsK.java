941cbc572d9d0fcd13a3b5b01b5abc02a6549402
Shuting Guan <afatcat@gmail.com>
2018-04-18 14:19:51 -0400

    MaximumSizeSubarraySumEqualsK

--- src/main/java/net/shutingg/leetCode/MaximumSizeSubarraySumEqualsK.java
+++ src/main/java/net/shutingg/leetCode/MaximumSizeSubarraySumEqualsK.java
@@ -7,6 +7,13 @@ import java.util.Map;
  * https://leetcode.com/problems/maximum-size-subarray-sum-equals-k/description/
  */
 public class MaximumSizeSubarraySumEqualsK {
+    /**
+     * Hash - O(n)
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
     public int maxSubArrayLen(int[] nums, int k) {
         if (nums == null || nums.length == 0) {
             return 0;
@@ -28,5 +35,31 @@ public class MaximumSizeSubarraySumEqualsK {
         }
 
         return max;
+    }
+
+    /**
+     * PrefixSum - O(n^2)
+     *
+     * @param nums: an array
+     * @param k: a target value
+     * @return: the maximum length of a subarray that sums to k
+     */
+    public int maxSubArrayLen2(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int[] prefixSum = new int[nums.length + 1];
+        for (int i = 1; i <= nums.length; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
+        }
+        for (int l = nums.length; l >= 1; l--) {
+            for (int i = 0; i + l <= nums.length; i++) {
+                if (prefixSum[i + l] - prefixSum[i] == k) {
+                    return l;
+                }
+            }
+        }
+        return 0;
     }
 }
