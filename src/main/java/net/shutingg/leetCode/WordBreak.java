a31413865832e8928de33c5d64b99dfea03b724d
Shuting Guan <afatcat@gmail.com>
2018-04-18 16:23:51 -0400

    WordBreak improved

--- src/main/java/net/shutingg/leetCode/WordBreak.java
+++ src/main/java/net/shutingg/leetCode/WordBreak.java
@@ -5,10 +5,46 @@ import java.util.List;
 import java.util.Set;
 
 /**
- * https://leetcode.com/problems/word-break/description/
+ *
  */
 public class WordBreak {
     /**
+     * http://www.lintcode.com/en/problem/word-break/
+     * DP with slight improvement
+     *
+     * @param s: A string
+     * @param dict: A dictionary of words dict
+     * @return: A boolean
+     */
+    public boolean wordBreak(String s, Set<String> dict) {
+        if (s == null || dict == null) {
+            return false;
+        }
+
+        int maxWordLength = 0;
+        for (String word : dict) {
+            if (word.length() > maxWordLength) {
+                maxWordLength = word.length();
+            }
+        }
+
+        int n = s.length();
+        boolean[] f = new boolean[n + 1];
+        f[0] = true;
+        for (int j = 0; j <= n; j++) {
+            for (int i = Math.max(0, j - maxWordLength); i < j; i++) {
+                if (f[i] && dict.contains(s.substring(i, j))) {
+                    f[j] = true;
+                    break;
+                }
+            }
+        }
+
+        return f[n];
+    }
+
+    /**
+     * https://leetcode.com/problems/word-break/description/
      * DP
      *
      * @param s
