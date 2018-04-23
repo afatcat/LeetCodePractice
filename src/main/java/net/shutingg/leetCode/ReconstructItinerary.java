5ec6c54bbbf5a303d1bc64f6a7762721f7baeba8
Shuting Guan <afatcat@gmail.com>
2018-04-22 23:39:44 -0400

    ReconstructItinerary

---
+++ src/main/java/net/shutingg/leetCode/ReconstructItinerary.java
@@ -0,0 +1,60 @@
+package net.shutingg.leetCode;
+
+import java.util.*;
+
+/**
+ * https://leetcode.com/problems/reconstruct-itinerary/description/
+ */
+public class ReconstructItinerary {
+    public List<String> findItinerary(String[][] tickets) {
+        List<String> res = new ArrayList<>();
+        if (tickets == null || tickets.length == 0 || tickets[0].length != 2) {
+            return res;
+        }
+
+        Map<String, List<String>> graph = new HashMap<>();
+        for (int i = 0; i < tickets.length; i++) {
+            graph.putIfAbsent(tickets[i][0], new ArrayList<>());
+            graph.get(tickets[i][0]).add(tickets[i][1]);
+        }
+
+        Map<String, List<Boolean>> visited = new HashMap<>();
+        for (String key : graph.keySet()) {
+            Collections.sort(graph.get(key));
+            visited.put(key, new ArrayList<>());
+            for (int i = 0; i < graph.get(key).size(); i++) {
+                visited.get(key).add(false);
+            }
+        }
+
+        res.add("JFK");
+        dfs(graph, visited, "JFK", res, tickets.length + 1);
+
+        return res;
+    }
+
+    private void dfs(Map<String, List<String>> graph, Map<String, List<Boolean>> visited, String cur, List<String> res, int n) {
+        if (res.size() == n) {
+            return;
+        }
+
+        List<String> list = graph.get(cur);
+        if (list == null) {
+            return;
+        }
+        for (int i = 0; i < list.size(); i++) {
+            if (visited.get(cur).get(i)) {
+                continue;
+            }
+            visited.get(cur).set(i, true);
+            String next = list.get(i);
+            res.add(next);
+            dfs(graph, visited, next, res, n);
+            if (res.size() == n) {
+                return;
+            }
+            visited.get(cur).set(i, false);
+            res.remove(res.size() - 1);
+        }
+    }
+}
