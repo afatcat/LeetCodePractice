0c9f2a1a15ada5e4be79680a4abfa77ff06e22ac
Shuting Guan <afatcat@gmail.com>
2018-04-23 22:39:48 -0400

    PerfectSquares improved

--- src/main/java/net/shutingg/leetCode/PerfectSquares.java
+++ src/main/java/net/shutingg/leetCode/PerfectSquares.java
@@ -2,22 +2,15 @@ package net.shutingg.leetCode;
 
 public class PerfectSquares {
     public int numSquares(int n) {
-        if(n<=0){
-            return 0;
-        }
-        int[] res = new int[n+1];
-        int root = 0;
-        for(int i=1; i<=n; i++){
-            res[i] = i;
-            if(((root+1)*(root+1)) ==i){
-                res[i] = 1;
-                root++;
-            }else{
-                for(int j = 1; j <= root; j++){
-                    res[i] = res[i] < res[i-j*j] + 1 ? res[i] : res[i-j*j] + 1;
-                }
+        int[] f = new int[n + 1];
+        f[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            f[i] = i;
+            for (int j = 0; j * j <= i; j++) {
+                f[i] = Math.min(f[i], f[i - j * j] + 1);
             }
         }
-        return res[n];
+
+        return f[n];
     }
 }
